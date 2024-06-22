6d9547f826e24de196e85f88421c007613ab3320
Rajeev <rajeevgayaaws@gmail.com>
2024-06-22 23:36:31 +0530

    first commit

---
+++ src/test/java/com/rajeev/demo/DemoApplicationTests.java
@@ -0,0 +1,26 @@
+package com.rajeev.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	private MockMvc mockMvc;
+
+	@Test
+	public void testGreetMessage() throws Exception{
+		String name="Rajeev";
+		mockMvc.perform(MockMvcRequestBuilders.get("/greeting/{name}", name))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.content().string("Hello "+name));
+
+	}
+}
